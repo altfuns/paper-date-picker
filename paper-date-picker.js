a11fc9b8b4e038180de178dd68c8cf70a953ef20
Ben Davis <bendavis78@gmail.com>
2015-03-02 02:12:57 -0600

    WIP

--- paper-date-picker.js
+++ paper-date-picker.js
@@ -1,3 +1,9 @@
+(function() {
+
+var $ = function(e){
+  return e.querySelector ? e.querySelector.bind(e) : document.querySelector(e);
+};
+
 Polymer("paper-date-picker", {
   once: function(obj, eventName, callback) {
     var doCallback = function() {
@@ -22,7 +28,6 @@ Polymer("paper-date-picker", {
     var element = e.originalTarget ? e.originalTarget : e.toElement;
   },
   ready: function() {
-    this.days = [];
     this.months = [];
     this.years = [];
     this.today = new Date();
@@ -34,9 +39,16 @@ Polymer("paper-date-picker", {
     this.localeChanged();
     this.startYear = this.startYear || this.today.getFullYear();
     this.endYear = this.endYear || this.today.getFullYear() + 20;
+    this.totalWeekRows = 0;
     this.populateCalendar();
     this.headingDate = this.value ? this.value : new Date();
-    this.scrollToDate(this.headingDate);
+
+    this.rafSets = [0, 1];
+    var rafDays = [];
+    for (var i=1; i<=31; i++) {
+      rafDays.push(i);
+    }
+    this.rafDays = rafDays;
 
     // track page transitions
     this.$.pages.addEventListener('core-animated-pages-transition-prepare', function() {
@@ -46,38 +58,218 @@ Polymer("paper-date-picker", {
       this._pageTransitioning = false;
     });
   },
+  domReady: function() {
+    this.initScroller();
+  },
   populateCalendar: function() {
-    var year, month, days, day, date = new Date();
+    // popluates the calendar data for all months in memory
+    var year, yearInfo, month, weekRows, startOn, day;
+    var top = 0;
+    var date = new Date();
     var thisYear = this.today.getFullYear();
     var thisMonth = this.today.getMonth();
     var thisDay = this.today.getDate();
 
     for (year=this.startYear; year<=this.endYear; year++) {
+      yearInfo = {year: year};
+      this.years.push(yearInfo);
+
       date.setYear(year);
       for (month=0; month<12; month++) {
-        days = [];
         day = 1;
+
+        // get starting weekday
         date.setMonth(month);
         date.setDate(1);
+        startOn = date.getDay();
         
-        // add "padding" days
-        for (d=0; d<date.getDay(); d++) {
-          days.push('');
+        // get number of days
+        date.setMonth(month + 1);
+        date.setDate(0);
+        numDays = date.getDate();
+
+        weekRows = Math.ceil(numDays / 7);
+        var days = [];
+        for (var i=0; i<numDays; i++) {
+          days.push(i+1);
         }
 
-        // add actual days 
-        while (date.getMonth() == month) {
-          days.push(day);
-          date.setDate(++day);
-        }
         monthData = {
+          yearInfo: yearInfo,
           year: year,
           month: month,
-          days: days
+          name: this.monthNames[month],
+          startOn: startOn,
+          numDays: numDays,
+          days: days,
+          rows: weekRows,
+          totalRows: this.totalWeekRows
         };
         this.months.push(monthData);
+        this.totalWeekRows += weekRows;
       }
-      this.years.push({year: year});
+    }
+  },
+  initScroller: function() {
+    var i, j, id;
+    var rafSets = 2;
+    var rafNodes = {};
+
+    this._monthIdx = 0;
+    this._monthScrollTop = this.$.calendarList.scrollTop;
+
+    // Create nodes for use in RAF
+    this.$.rafNodes.$ = rafNodes;
+    this._monthNodes = this.$.calendarList.querySelectorAll('.month');
+    this._initialViewportSize = this.$.calendarListViewPort.offsetHeight;
+
+    var mapId = function(id, sets) {
+      var n = sets === false ? 1 : rafSets;
+      var rafId;
+      for (var i=0; i<n; i++) {
+        rafId = 'raf' + (sets === false ? id : i + id);
+        rafNodes[rafId] = this.$.rafNodes.querySelector('#' + rafId);
+      }
+    }.bind(this);
+
+    mapId('Month');
+  
+    for (i=1; i<=31; i++) {
+        mapId('Day' + i);
+    }
+
+    var monthNameWidths = {};
+    for (i=0; i<12; i++) {
+      id = 'MonthName' + this.monthNames[i];
+      mapId(id, false);
+      monthNameWidths[i] = rafNodes['raf' + id].offsetWidth;
+    }
+
+    for (i=0; i<this.years.length; i++) {
+      var y = this.years[i];
+      id = 'Year' + y.year;
+      mapId(id);
+      y.width = rafNodes['raf0Year' + y.year].offsetWidth;
+    }
+
+    // calculate dimensions and positioning of month elements
+    var calcMonth = rafNodes.raf0Month;
+    var calcMonthStyle = getComputedStyle(calcMonth);
+    var padding = parseInt(calcMonthStyle.paddingLeft.replace('px', ''));
+    var calcTitle = calcMonth.querySelector('.month-title');
+    var titleStyle = getComputedStyle(calcTitle);
+    var calcDay = rafNodes.raf0Day1;
+    var dayHeight = calcDay.offsetHeight;
+    var dayWidth = calcDay.offsetWidth;
+    var titleHeight = calcTitle.offsetHeight;
+    var titleCenter = calcTitle.offsetWidth / 2;
+    var daysOffset = titleHeight + calcMonth.querySelector('.month-weekdays').offsetHeight;
+
+    this._dayHeight = dayHeight;
+    this._daysOffset = daysOffset;
+    this._titleHeight = titleHeight;
+    this._dayWidth = dayWidth;
+    this._monthPadding = padding;
+
+    var top = 0;
+    for (i=0; i<this.months.length; i++) {
+      var month = this.months[i];
+      var monthNameWidth = monthNameWidths[month.month];
+      var titleWidth = monthNameWidth + month.yearInfo.width;
+      var nameLeft = titleCenter - (titleWidth / 2);
+      month.top = top;
+      month.height = daysOffset + (month.rows * dayHeight);
+      month.nameLeft = padding + nameLeft;
+      month.yearLeft = padding + nameLeft + monthNameWidth;
+      top += month.height;
+    }
+
+    // Set the RAF handler
+    this._boundRAFHandler = this.updateMonthScroller.bind(this);
+
+    // Set the scroll handler
+    var scrolling = false;
+    this.$.calendarList.addEventListener('scroll', function(){
+      var scrollTop = this.$.calendarList.scrollTop;
+      var viewport = this.$.calendarListViewPort;
+      this._monthIdx = this.getMonthIndex(this._monthScrollTop);
+      this._monthScrollTop = scrollTop;
+
+      // add more viewport size if needed
+      if ((scrollTop + this._initialViewportSize) > viewport.offsetHeight) {
+        viewport.offsetHeight += this._initialViewportSize;
+      }
+
+      // offload layout onto RAF
+      if (!scrolling) {
+        requestAnimationFrame(this._boundRAFHandler);
+      }
+      scrolling = true;
+    }.bind(this));
+
+    this.updateMonthScroller();
+  },
+  getMonthIndex: function(pos) {
+    var idx, month, bottom, height;
+    // TODO: this could possibly be optimized using some clever math
+    min = Math.floor(pos / ((this._dayHeight * 6) + this._titleHeight));
+    max = Math.ceil(pos / ((this._dayHeight * 4) + this._titleHeight));
+    while (min <= max) {
+      idx = (min + max) / 2 | 0;
+      month = this.months[idx];
+      if (bottom < pos) {
+        min = idx + 1;
+      }
+      else if ((bottom - height) > pos) {
+        max = idx - 1;
+      }
+      else {
+        return idx;
+      }
+    }
+    return -1;
+  },
+  updateMonthScroller: function() {
+    // WARNING: runs in RAF, do not trigger reflows or unecessary repaints
+    // Get first month that would show in the viewport
+    // lay out this month and the next
+    this.layoutMonth(0);
+    this.layoutMonth(1);
+  },
+  layoutMonth: function(n) {
+    // WARNING: runs in RAF, do not trigger reflows or unecessary repaints
+    var rafNodes = this.$.rafNodes.$;
+    var month = this.months[this._monthIdx + n];
+    var rafMonth = rafNodes['raf' + n + 'Month'];
+
+    // layout title
+    var titleMonth = rafNodes['rafMonthName' + month.name];
+    var titleYear = rafNodes['raf' + n + 'Year' + month.year];
+    var top = month.top;
+    var top_px = top + 'px';
+    rafMonth.style.top = top_px;
+    titleMonth.style.top = top_px;
+    titleYear.style.top = top_px;
+    titleMonth.style.left = month.nameLeft + 'px';
+    titleYear.style.left = month.yearLeft + 'px';
+
+    // layout days
+    var padding = this._monthPadding;
+    var dayHeight = this._dayHeight;
+    var dayWidth = this._dayWidth;
+    var dayTop = top + this._daysOffset;
+    var dayLeft = padding;
+    var startOn = month.startOn;
+    var numDays = month.numDays;
+    
+    var cell, col, row;
+    for (var i=0; i<numDays; i++) {
+      cell = startOn + i;
+      col = cell % 7;
+      row = Math.floor(cell / 7);
+      day = rafNodes['raf' + n + 'Day' + (i+1)]; 
+      day.style.top = dayTop + (row * dayHeight) + 'px';
+      day.style.left = padding + (col * dayWidth) + 'px';
     }
   },
   getMonthIdx: function(year, month) {
@@ -199,3 +391,5 @@ PolymerExpressions.prototype.dateFormat = function(date, format) {
   if (!date) return '';
   return moment(date).format(format);
 };
+
+})();
