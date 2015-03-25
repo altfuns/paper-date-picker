14aed89ba6434a4e1e4e774e2c3b42887ebe4eb5
Ben Davis <bendavis78@gmail.com>
2015-03-24 22:29:22 -0500

    WIP

--- paper-date-picker.js
+++ paper-date-picker.js
@@ -1,89 +1,88 @@
-Polymer("paper-date-picker", {
-  once: function(obj, eventName, callback) {
-    var doCallback = function() {
-      callback.apply(obj, arguments);
-      obj.removeEventListener(eventName, doCallback);
-    };
-    obj.addEventListener(eventName, doCallback);
-  },
-  publish: {
-    locale: null,
-    minYear: null,
-    maxYear: null,
-    showHeading: true,
-    narrow: {type: 'boolean', value: false, reflect: true},
-    isTouch: {type: 'boolean', value: false, reflect: true}
-  },
-  dateClicked: function(e) {
-    var element = e.originalTarget ? e.originalTarget : e.toElement;
-  },
-  ready: function() {
-    this.today = this.$.calendar.today;
-    this.isTouch = 'ontouchstart' in window;
-    this.narrow = false;
-    this.localeChanged();
-    this.headingDate = this.value ? this.value : new Date();
-    this.years = this.$.calendar.years;
+(function() {
+  var CALENDAR = 0
+  var YEARLIST = 1
 
-    // track page transitions
-    this.$.pages.addEventListener('core-animated-pages-transition-prepare', function() {
-      this._pageTransitioning = true;
-    });
-    this.$.pages.addEventListener('core-animated-pages-transition-end', function() {
-      this._pageTransitioning = false;
-    });
-  },
-  tapHeadingDay: function() {
-    if (this.$.pages.selected !== 0) {
-      this.selectPage(0, function() {
-        this.$.calendar.scrollToDate(this.headingDate);
-      }.bind(this));
-    } else {
+  Polymer("paper-date-picker", {
+    publish: {
+      locale: null,
+      minYear: null,
+      maxYear: null,
+      showHeading: true,
+      narrow: {type: 'boolean', value: false, reflect: true},
+      isTouch: {type: 'boolean', value: false, reflect: true}
+    },
+    dateClicked: function(e) {
+      var element = e.originalTarget ? e.originalTarget : e.toElement;
+    },
+    ready: function() {
+      this.today = this.$.calendar.today;
+      this.isTouch = 'ontouchstart' in window;
+      this.narrow = false;
+      this.localeChanged();
+      this.headingDate = this.value ? this.value : new Date();
+      this.years = this.$.calendar.years;
+    },
+    tapHeadingDay: function() {
       this.$.calendar.scrollToDate(this.headingDate);
-    }
-  },
-  tapHeadingMonth: function() {
-    this.tapHeadingDay();
-  },
-  tapHeadingYear: function() {
-    var year = this.headingDate.getFullYear();
-    this.scrollYearList(year);
-    this.selectPage(1);
-  },
-  selectPage: function(page, callback) {
-    if (this._pageTransitioning) return;
-    if (callback) {
-      this.once(this.$.pages, 'core-animated-pages-transition-end', callback);
-    }
-    this.$.pages.selected = page;
-  },
-  scrollYearList: function(year, cb) {
-    this.async(function() {
-      var idx = year - this.startYear;
-      this.$.yearList.scrollToItem(idx);
-      this.$.yearList.scrollTop -= 94;
-      if(cb) cb();
-    });
-  },
-  dateSelected: function(e, detail) {
-    this.value = detail.date;
-  },
-  valueChanged: function(oldValue, newValue) {
-    if (newValue) {
-      this.headingDate = newValue;
-    } else {
-      this.headingDate = this.today;
-    }
-  },
-  // attributes that propagate to paper-calendar
-  localeChanged: function() {
-    moment.locale(this.locale);
-    this.$.calendar.locale = this.locale;
-  },
-  minYearChanged: function() {
-    this.$.calendar.minYear = this.minYear;
-  },
-  maxYearChanged: function() {
-    this.$.calendar.maxYear = this.maxYear;
-  }
-});
+      if (this.$.pages.selected !== CALENDAR) {
+        this.selectPage(CALENDAR);
+      }
+    },
+    tapHeadingMonth: function() {
+      this.tapHeadingDay();
+    },
+    tapHeadingYear: function() {
+      var year = this.headingDate.getFullYear();
+      this.scrollToYear(year);
+      if (this.$.pages.selected !== YEARLIST) {
+        this.$.yearSelector.selected = year.toString();
+        this.selectPage(YEARLIST);
+      }
+    },
+    selectPage: function(page, callback) {
+      this.$.pages.selected = page;
+    },
+    scrollToYear: function(year) {
+      this.async(function() {
+        var el = this.$.yearList.querySelector('.year[label="' + year + '"]')
+        this.$.yearList.scrollTop = el.offsetTop - 94;
+      });
+    },
+    yearSelected: function(e, detail) {
+      var year = parseInt(detail.item.getAttribute('label'));
+      var headingDate = new Date(this.headingDate)
+      if (year !== headingDate.getFullYear()) {
+        this.async(function() {
+          headingDate.setFullYear(year);
+          this.headingDate = headingDate;
+        });
+        this.$.calendar.populate(year);
+        this.async(function() {
+          this.$.calendar.scrollToDate(this.headingDate);
+          this.selectPage(CALENDAR)
+        });
+      }
+    },
+    dateSelected: function(e, detail) {
+      this.value = detail.date;
+    },
+    valueChanged: function(oldValue, newValue) {
+      if (newValue) {
+        this.headingDate = newValue;
+      } else {
+        this.headingDate = this.today;
+      }
+    },
+    // attributes that propagate to paper-calendar
+    localeChanged: function() {
+      moment.locale(this.locale);
+      this.$.calendar.locale = this.locale;
+    },
+    minYearChanged: function() {
+      this.$.calendar.minYear = this.minYear;
+    },
+    maxYearChanged: function() {
+      this.$.calendar.maxYear = this.maxYear;
+    },
+  });
+})();
